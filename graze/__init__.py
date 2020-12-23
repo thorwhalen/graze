8631a33e5f7602750dff83a2f11a003a4dba58fa
Thor Whalen <thorwhalen1@gmail.com>
2020-12-23 13:08:18 -0800

    - GrazeWithDataRefresh: Like Graze, but where you can specify a time_to_live "freshness threshold" to trigger the re-download of data - A function to access Graze and GrazeWithDataRefresh for one request only.

--- graze/__init__.py
+++ graze/__init__.py
@@ -1,22 +1,18 @@
-from py2store import QuickBinaryStore, LocalBinaryStore
-# from py2store.slib.s_zipfile import ZipFilesReaderAndBytesWriter
+import time
+import os
+from warnings import warn
+import requests
+from typing import Union, Optional
+
+from py2store import LocalBinaryStore
 from py2store.persisters.local_files import ensure_slash_suffix
 from py2store.caching import mk_sourced_store
-# from py2store.filesys import mk_relative_path_store, LocalFileDeleteMixin
 from py2store.stores.local_store import AutoMkDirsOnSetitemMixin, LocalJsonStore
 from py2store.trans import add_ipython_key_completions, wrap_kvs
-# from py2store.key_mappers.paths import str_template_key_trans
-# from py2store.appendable import appendable
-import os
+from py2store.dig import inner_most_key
 
-import requests
-
+# TODO: handle configuration and existence of root
 DFLT_GRAZE_DIR = os.path.expanduser('~/graze')
-
-#
-# @mk_relative_path_store(prefix_attr='rootdir')
-# class RelZipFiles(ZipFilesReaderAndBytesWriter, LocalFileDeleteMixin):
-#     pass
 
 
 def clog(condition, *args):
@@ -49,14 +45,14 @@ class LocalGrazed(AutoMkDirsOnSetitemMixin, LocalBinaryStore):
 
 class Internet:
     def __getitem__(self, k):
+        if k.endswith('/'):
+            k = k[:-1]  # because it shouldn't matter as url (?) and having it leads to dirs (not files) being created
         resp = requests.get(k)
         if resp.status_code == 200:
             return resp.content
         else:
             raise KeyError(f"Response code was {resp.status_code}")
 
-
-# kaggle_remote_datasets_bytes = kv_wrap(remote_key_trans)(KaggleBytesDatasetReader)()
 
 Graze = mk_sourced_store(
     store=LocalGrazed,
@@ -66,3 +62,54 @@ Graze = mk_sourced_store(
     __module__=__name__
 )
 
+A_WEEK_IN_SECONDS = 7 * 24 * 60 * 60  # one week
+
+
+class GrazeWithDataRefresh(Graze):
+    def __init__(self,
+                 time_to_live: Union[int, float] = A_WEEK_IN_SECONDS,
+                 on_error: str = 'warn'):
+        """Like Graze, but where you can specify a time_to_live "freshness threshold" to trigger the re-download of data
+
+        :param time_to_live: In seconds.
+        :param on_error: What to do if there's an error when fetching the new data.
+            'raise' raise an error (but keep the cached data)
+            'warn' warn the user of the stale data (but return anyway)
+            'ignore' ignore the error, and return the stale data
+        """
+        super().__init__()
+        self.time_to_live = time_to_live
+        self.on_error = on_error
+
+    def __getitem__(self, k):
+        v = None
+        if k in self:
+            # TODO: Use info store that is not necessarily a local files sys?
+            filepath = inner_most_key(self, k)
+            age = (time.time() - os.stat(filepath).st_mtime)  # Note: local file sys cache is assumed here!
+            if age > self.time_to_live:
+                # Need to refresh the data...
+                try:
+                    # TODO: perhaps do this with super()
+                    v = Internet()[k]
+                    with open(filepath, 'wb') as f:
+                        f.write(v)  # replace existing
+                except Exception as e:
+                    if self.on_error == 'raise':
+                        raise
+                    elif self.on_error == 'warn':
+                        warn(f"There was an error getting a fresh copy of {k}, "
+                             f"so I'll give you a copy that's {age} seconds old. "
+                             f"The error was {e}")
+        if v is None:
+            v = super().__getitem__(k)  # retrieve the data normally
+        return v
+
+
+def graze(url: str,
+          max_age: Optional[Union[int, float]] = None):
+    """Get the contents of the url (persisting the results in a local file, for next time you'll ask for it)"""
+    if max_age is None:
+        return Graze()[url]
+    else:
+        return GrazeWithDataRefresh(time_to_live=max_age)[url]
